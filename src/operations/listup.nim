87376cc3f2db088ef0218402c311699e7b614eb5
Juraj Kollár <mostypc7@gmail.com>
2026-03-15 20:13:12 +0100

    Update listup.nim

--- src/operations/listup.nim
+++ src/operations/listup.nim
@@ -1,11 +1,12 @@
 import os
+import strutils
 import ../color
 
 proc listup*() =
   log_info("updating package list")
   var mirror = ""
   try:
-    mirror = readFile("/etc/car/mirror")
+    mirror = readFile("/etc/car/mirror").strip()
   except:
     log_error("no mirror is set. did you run 'car init'?")
     quit()
@@ -13,6 +14,12 @@ proc listup*() =
   if exit != 0:
     log_error("failed to update package list")
     quit()
-  else:
-    log_ok("package list updated")
-    quit() # i have no clue why didnt this quit by itself
+  if execShellCmd("cat /etc/car_propiertary.lock 2>/dev/null") == 0:
+    # propiertary enabled
+    log_info("updating propriertary repo")
+    let exit = execShellCmd("curl -s -L https://github.com/redroselinux/car-propiertary-repo/raw/refs/heads/main/README >> /etc/car/packagelist")
+    if exit != 0:
+      log_error("failed to update package list")
+      quit()
+  log_ok("package list updated")
+  quit() # i have no clue why didnt this quit by itself
