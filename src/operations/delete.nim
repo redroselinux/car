698182e8a933739014aa3ad9b2e553d64dd5d440
Juraj Kollár <mostypc7@gmail.com>
2026-03-17 07:38:30 +0100

    Update delete.nim

--- src/operations/delete.nim
+++ src/operations/delete.nim
@@ -6,38 +6,35 @@ import ../color
 
 proc delete*(packages: seq[string]) =
   let start = getTime()
-  let reproPath = "/etc/repro.car"
-
-  var reproLines = readFile(reproPath).splitLines()
+  var reproLines = readFile("/etc/repro.car").splitLines()
 
   for pkg in packages:
-    let savePath = "/etc/car/saves/" & pkg
+    if pkg & "=" in reproLines.join():
+      if fileExists("/etc/car/saves/" & pkg):
+        let savedFiles = readFile("/etc/car/saves/" & pkg).splitLines()
 
-    if fileExists(savePath):
-      let savedFiles = readFile(savePath).splitLines()
+        for line in savedFiles:
+          let target = line.strip()
 
-      for line in savedFiles:
-        let target = line.strip()
+          if target == "" or target == "car":
+            continue
 
-        if target == "" or target == "car":
-          continue
+          if fileExists(target):
+            try:
+              removeFile(target)
+            except OSError:
+              log_error("permission denied or error deleting: " & target)
+          else:
+            continue
 
-        if fileExists(target):
-          try:
-            removeFile(target)
-          except OSError:
-            log_error("permission denied or error deleting: " & target)
-        else:
-          continue
+        removeFile("/etc/car/saves/" & pkg)
+      else:
+        log_warn("no tracking file found for: " & pkg)
+        return
 
-      removeFile(savePath)
-    else:
-      log_warn("no tracking file found for: " & pkg)
-      return
+      reproLines.keepItIf(not it.startsWith(pkg & "="))
 
-    reproLines.keepItIf(not it.startsWith(pkg & "="))
-
-  writeFile(reproPath, reproLines.join("\n"))
+  writeFile("/etc/repro.car", reproLines.join("\n"))
 
   let elapsed = getTime() - start
   log_ok("process complete in " & $elapsed.inMilliseconds & " ms")
