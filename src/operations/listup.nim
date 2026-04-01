1cba67217b744660347fe4bff625c61fa9a0f047
Juraj Kollár <mostypc7@gmail.com>
2026-04-01 21:02:17 +0200

    Update listup.nim

--- src/operations/listup.nim
+++ src/operations/listup.nim
@@ -3,6 +3,8 @@ import strutils
 import ../color
 
 proc listup*() =
+  # TODO: custom repos
+
   log_info("updating package list")
   var mirror = ""
   try:
@@ -10,15 +12,13 @@ proc listup*() =
   except:
     log_error("no mirror is set. did you run 'car init'?")
     quit()
-  let exit = execShellCmd("curl -s -L -o /etc/car/packagelist " & mirror)
-  if exit != 0:
+  if execShellCmd("curl -s -L -o /etc/car/packagelist " & mirror) != 0:
     log_error("failed to update package list")
     quit()
   if execShellCmd("cat /etc/car_propiertary.lock 2>/dev/null") == 0:
     # propiertary enabled
     log_info("updating propriertary repo")
-    let exit = execShellCmd("curl -s -L https://github.com/redroselinux/car-propiertary-repo/raw/refs/heads/main/README >> /etc/car/packagelist")
-    if exit != 0:
+    if execShellCmd("curl -s -L https://github.com/redroselinux/car-propiertary-repo/raw/refs/heads/main/README >> /etc/car/packagelist") != 0:
       log_error("failed to update package list")
       quit()
   log_ok("package list updated")
