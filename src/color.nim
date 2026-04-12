45eaaf64c7c5f7f13558b26218201c52030a9c57
Juraj Kollár <mostypc7@gmail.com>
2026-04-12 11:12:11 +0200

    massive i/o and fn call overhead removal

--- src/color.nim
+++ src/color.nim
@@ -1,20 +1,34 @@
-proc log_error*(message: string) =
-  echo "\e[1m\e[91merror\e[0m: " & message
+proc log_error*(msg: string) {.inline.} =
+  stdout.write("\e[1m\e[91merror\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
 
-proc log_warn*(message: string) =
-  echo "\e[1m\e[93mwarning\e[0m: " & message
+proc log_warn*(msg: string) {.inline.} =
+  stdout.write("\e[1m\e[93mwarning\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
 
-proc log_ok*(message: string) =
-  echo "\e[1m\e[92mok\e[0m: " & message
+proc log_ok*(msg: string) {.inline.} =
+  stdout.write("\e[1m\e[92mok\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
 
-proc log_done*(message: string) =
-  echo "\e[1m\e[92mdone\e[0m: " & message
+proc log_done*(msg: string) {.inline.} =
+  stdout.write("\e[1m\e[92mdone\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
 
-proc log_info*(message: string) =
-  echo "\e[1m\e[94minfo\e[0m: " & message
+proc log_info*(msg: string) {.inline.} =
+  stdout.write("\e[1m\e[94minfo\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
 
-proc log_pick*(message: string) =
-  echo "\e[1;36mpick\e[0m: " & message
+proc log_pick*(msg: string) {.inline.} =
+  stdout.write("\e[1;36mpick\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
 
-proc log_option*(message: string) =
-  echo "\e[1;30moption\e[0m: " & message
+proc log_option*(msg: string) {.inline.} =
+  stdout.write("\e[1;30moption\e[0m: ")
+  stdout.write(msg)
+  stdout.write("\n")
