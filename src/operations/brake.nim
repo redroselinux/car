fc5f4c868c0dfe6d9a1a2480d1eea16fa1c4a909
mostypc123 <mostypc7@gmail.com>
2026-05-24 15:49:37 +0200

    fix

--- src/operations/brake.nim
+++ src/operations/brake.nim
@@ -7,9 +7,9 @@ proc brakePackages*(packages: seq[string]) =
       log_error "Package " & i & " is not installed."
       quit(1)
     if fileExists("/etc/car/saves/" & i & "-brake"):
-      log_error "Package " & i & " is is already braked."
+      log_error "Package " & i & " is already braked."
       quit(1)
-    writeFile("/etc/car/saves/" & i & "-brake", "This package is braked; it does not recieve updates.")
+    writeFile("/etc/car/saves/" & i & "-brake", "This package is braked; it does not receive updates.")
     log_ok "Braked " & i
 
 proc releasePackages*(packages: seq[string]) =
@@ -20,5 +20,5 @@ proc releasePackages*(packages: seq[string]) =
     if not fileExists("/etc/car/saves/" & i & "-brake"):
       log_error "Package " & i & " is not braked."
       quit(1)
-  removeFile("/etc/car/saves/" & i & "-brake")
-  log_ok "Released " & i
+    removeFile("/etc/car/saves/" & i & "-brake")
+    log_ok "Released " & i
